d2a9b91d6d45090ebd064b7a3b3356ae67742085
Mohd Norhaimi <haimiyahya@gmail.com>
2023-08-09 11:41:58 +0800

    add parse tr31 functionality

--- lib/parse_tr31_keyblock_format.ex
+++ lib/parse_tr31_keyblock_format.ex
@@ -3,17 +3,109 @@ defmodule ParseTr31KeyblockFormat do
   Documentation for `ParseTr31KeyblockFormat`.
   """
 
-  @doc """
-  Hello world.
+  def parse(entire_key_block) do
 
-  ## Examples
+    result = []
 
-      iex> ParseTr31KeyblockFormat.hello()
-      :world
+    {p_ver, the_rest} = match_ver(entire_key_block)
+    # IO.inspect "ver: #{p_ver}"
+    result = Keyword.put(result, :ver, p_ver)
 
-  """
-  def hello do
-    :world
+    {p_block_length, the_rest} = match_block_length(the_rest)
+    # IO.inspect "block_length: #{p_block_length}"
+    result = Keyword.put(result, :block_length, p_block_length)
+
+    {p_key_usage, the_rest} = match_key_usage(the_rest)
+    # IO.inspect "key_usage: #{p_key_usage}"
+    result = Keyword.put(result, :key_usage, p_key_usage)
+
+    {p_algorithm, the_rest} = match_algorithm(the_rest)
+    # IO.inspect "algorithm: #{p_algorithm}"
+    result = Keyword.put(result, :algorithm, p_algorithm)
+
+    {p_mode_of_use, the_rest} = match_mode_of_use(the_rest)
+    # IO.inspect "mode_of_use: #{p_mode_of_use}"
+    result = Keyword.put(result, :mode_of_use, p_mode_of_use)
+
+    {p_version_number, the_rest} = match_version_number(the_rest)
+    # IO.inspect "version_number: #{p_version_number}"
+    result = Keyword.put(result, :version_number, p_version_number)
+
+    {p_exportibility, the_rest} = match_exportibility(the_rest)
+    # IO.inspect "exportibility: #{p_exportibility}"
+    result = Keyword.put(result, :exportibility, p_exportibility)
+
+    {p_optional_blocks, the_rest} = match_optional_blocks(the_rest)
+    # IO.inspect "optional_blocks: #{p_optional_blocks}"
+    result = Keyword.put(result, :optional_blocks, p_optional_blocks)
+
+    {p_reserved, the_rest} = match_reserved(the_rest)
+    # IO.inspect "reserved: #{p_reserved}"
+    result = Keyword.put(result, :reserved, p_reserved)
+
+    {p_key_length, the_rest} = match_key_length(the_rest)
+    # IO.inspect "key_length: #{p_key_length}"
+    result = Keyword.put(result, :key_length, p_key_length)
+
+    {p_key, the_rest} = match_key(p_key_length, the_rest)
+    # IO.inspect "key: #{p_key}"
+    result = Keyword.put(result, :key, p_key)
+
+    {p_mac, the_rest} = match_mac(the_rest)
+    # IO.inspect "mac: #{p_mac}"
+    result = Keyword.put(result, :mac, p_mac)
+
+    result
+
+  end
+
+  def match_ver(<<version::binary-size(1), the_rest::binary>>) when version in ~w(A B C) do
+    {version, the_rest}
+  end
+
+  def match_block_length(<<_::4, a::4, _::4, b::4, _::4, c::4, _::4, d::4, the_rest::binary>>) do
+    {a*1000 + b*100 + c*10 + d, the_rest}
+  end
+
+  def match_key_usage(<<key_usage::binary-size(2), the_rest::binary>>) when key_usage in ~w(D0 M0 M1 M2 M3 M4 M5 P0) do
+    {key_usage, the_rest}
+  end
+
+  def match_algorithm(<<algorithm::binary-size(1), the_rest::binary>>) when algorithm in ~w(A D E H R S T) do
+    {algorithm, the_rest}
+  end
+
+  def match_mode_of_use(<<mode_of_use::binary-size(1), the_rest::binary>>) when mode_of_use in ~w(B C D E G N S V X) do
+    {mode_of_use, the_rest}
+  end
+
+  def match_version_number(<<version_number::binary-size(2), the_rest::binary>>) do
+    {version_number, the_rest}
+  end
+
+  def match_exportibility(<<exportibility::binary-size(1), the_rest::binary>>) do
+    {exportibility, the_rest}
+  end
+
+  def match_optional_blocks(<<optional_blocks::binary-size(2), the_rest::binary>>) do
+    {optional_blocks, the_rest}
+  end
+
+  def match_reserved(<<reserved::binary-size(2), the_rest::binary>>) do
+    {reserved, the_rest}
+  end
+
+  def match_key_length(<<_::4, a::4, _::4, b::4, the_rest::binary>>) do
+    {a*10+b, the_rest}
+  end
+
+  def match_key(key_size, data) do
+    <<key::binary-size(key_size), the_rest::binary>> = data
+    {key, the_rest}
+  end
+
+  def match_mac(<<mac::binary-size(8), the_rest::binary>>) do
+    {mac, the_rest}
   end
 
 
--- test/parse_tr31_keyblock_format_test.exs
+++ test/parse_tr31_keyblock_format_test.exs
@@ -27,7 +27,7 @@ defmodule ParseTr31KeyblockFormatTest do
 
     key_length = :rand.uniform(36) # 72 is the max key length (I think in ascii hex)
     key_value = :crypto.strong_rand_bytes(key_length) |> Base.encode16
-    key_length = key_length |> to_string
+    key_length = key_length |> to_string|> String.pad_leading(2, "0")
 
     mac = :crypto.strong_rand_bytes(4) |> Base.encode16
 
@@ -66,75 +66,86 @@ defmodule ParseTr31KeyblockFormatTest do
     reserved = "00"
 
     key_length = :rand.uniform(36) # 72 is the max key length (I think in ascii hex)
-    key_value = :crypto.strong_rand_bytes(key_length) |> Base.encode16
-    key_length = key_length |> to_string
+    # IO.inspect "key_length: #{key_length*2}"
+    key = :crypto.strong_rand_bytes(key_length) |> Base.encode16
+    key_length = key_length*2 |> to_string |> String.pad_leading(2, "0")
 
     mac = :crypto.strong_rand_bytes(4) |> Base.encode16
 
     key_usage_till_mac =
       key_usage <> algorithm <> mode_of_use <> version_number
-      <> exportibility <> optional_blocks <> reserved <> key_length <> key_value <> mac
+      <> exportibility <> optional_blocks <> reserved <> key_length <> key <> mac
 
-    key_block_length = 1 + String.length(key_usage_till_mac) |> to_string |> String.pad_leading(4, "0")
+    block_length = 1 + String.length(key_usage_till_mac) |> to_string |> String.pad_leading(4, "0")
 
-    entire_key_block = ver <> key_block_length <> key_usage_till_mac
+    entire_key_block = ver <> block_length <> key_usage_till_mac
 
-    {ver, the_rest} = match_ver(entire_key_block)
-    IO.inspect ver
-    {block_length, the_rest} = match_block_length(the_rest)
-    IO.inspect block_length
-    {key_usage, the_rest} = match_key_usage(the_rest)
-    IO.inspect key_usage
+    # IO.inspect entire_key_block
+
+    # {p_ver, the_rest} = match_ver(entire_key_block)
+    # # IO.inspect "ver: #{p_ver}"
+    # {p_block_length, the_rest} = match_block_length(the_rest)
+    # # IO.inspect "block_length: #{p_block_length}"
+    # {p_key_usage, the_rest} = match_key_usage(the_rest)
+    # # IO.inspect "key_usage: #{p_key_usage}"
+    # {p_algorithm, the_rest} = match_algorithm(the_rest)
+    # # IO.inspect "algorithm: #{p_algorithm}"
+    # {p_mode_of_use, the_rest} = match_mode_of_use(the_rest)
+    # # IO.inspect "mode_of_use: #{p_mode_of_use}"
+    # {p_version_number, the_rest} = match_version_number(the_rest)
+    # # IO.inspect "version_number: #{p_version_number}"
+    # {p_exportibility, the_rest} = match_exportibility(the_rest)
+    # # IO.inspect "exportibility: #{p_exportibility}"
+    # {p_optional_blocks, the_rest} = match_optional_blocks(the_rest)
+    # # IO.inspect "optional_blocks: #{p_optional_blocks}"
+    # {p_reserved, the_rest} = match_reserved(the_rest)
+    # # IO.inspect "reserved: #{p_reserved}"
+    # {p_key_length, the_rest} = match_key_length(the_rest)
+    # # IO.inspect "key_length: #{p_key_length}"
+    # {p_key, the_rest} = match_key(p_key_length, the_rest)
+    # # IO.inspect "key: #{p_key}"
+    # {p_mac, the_rest} = match_mac(the_rest)
+    # # IO.inspect "mac: #{p_mac}"
+    result = ParseTr31KeyblockFormat.parse(entire_key_block)
+
+    # IO.inspect result
+
+    p_ver = Keyword.get(result, :ver)
+    p_block_length = Keyword.get(result, :block_length)
+    p_key_usage = Keyword.get(result, :key_usage)
+    p_algorithm = Keyword.get(result, :algorithm)
+    p_mode_of_use = Keyword.get(result, :mode_of_use)
+    p_version_number = Keyword.get(result, :version_number)
+    p_exportibility = Keyword.get(result, :exportibility)
+    p_optional_blocks = Keyword.get(result, :optional_blocks)
+    p_reserved = Keyword.get(result, :reserved)
+    p_key_length = Keyword.get(result, :key_length)
+    p_key = Keyword.get(result, :key)
+    p_mac = Keyword.get(result, :mac)
+
+    assert Enum.member?(~w(A B C), p_ver)
+    assert Enum.member?(~w(D0 M0 M1 M2 M3 M4 M5 P0), p_key_usage)
+    assert Enum.member?(~w(A D E H R S T), p_algorithm)
+    assert Enum.member?(~w(B C D E G N S V X), p_mode_of_use)
+    assert Enum.member?(~w(E N S), p_exportibility)
+
+    assert p_ver == ver
+    {block_length, _} = Integer.parse(block_length)
+    assert p_block_length == block_length
+    assert p_key_usage == key_usage
+    assert p_algorithm == algorithm
+    assert p_mode_of_use == mode_of_use
+    assert p_version_number == version_number
+    assert p_exportibility == exportibility
+    assert p_optional_blocks == optional_blocks
+    assert p_reserved == reserved
+    {key_length, _} = Integer.parse(key_length)
+    assert p_key_length == key_length
+    assert p_key == key
+    assert p_mac == mac
 
   end
 
-  def match_ver(<<version::binary-size(1), the_rest::binary>>) when version in ~w(A B C) do
-    {version, the_rest}
-  end
 
-  def match_block_length(<<_::4, a::4, _::4, b::4, _::4, c::4, _::4, d::4, the_rest::binary>>) do
-    {a*1000 + b*100 + c*10 + d, the_rest}
-  end
-
-  def match_key_usage(<<key_usage::binary-size(2), the_rest::binary>>) when key_usage in ~w(D0 M0 M1 M2 M3 M4 M5 P0) do
-    {key_usage, the_rest}
-  end
-
-  def match_algorithm(<<algorithm::binary-size(1), the_rest::binary>>) when algorithm in ~w(A D E H R S T) do
-    {algorithm, the_rest}
-  end
-
-  def match_mode_of_use(<<mode_of_use::binary-size(1), the_rest::binary>>) when mode_of_use in ~w(B C D E G N S V X) do
-    {mode_of_use, the_rest}
-  end
-
-  def match_version_number(<<version_number::binary-size(2), the_rest::binary>>) do
-    {version_number, the_rest}
-  end
-
-  def match_exportibility(<<exportibility::binary-size(2), the_rest::binary>>) do
-    {exportibility, the_rest}
-  end
-
-  def match_optional_blocks(<<optional_blocks::binary-size(2), the_rest::binary>>) do
-    {optional_blocks, the_rest}
-  end
-
-  def match_reserved(<<reserved::binary-size(2), the_rest::binary>>) do
-    {reserved, the_rest}
-  end
-
-  def match_key_length(<<_::4, a::4, _::4, b::4, the_rest::binary>>) do
-    {a*10+b, the_rest}
-  end
-
-  def match_key(key_size, data) do
-    <<key::binary-size(key_size), the_rest::binary>> = data
-    {key, the_rest}
-  end
-
-  def match_mac(<<mac::binary-size(4), the_rest::binary>>) do
-    {mac, the_rest}
-  end
 
 end
